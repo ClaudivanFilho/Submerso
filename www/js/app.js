7954161224eeaa4234876dc3ca70262e797dfd03
Abner M. C. Araujo <abner.math.c@gmail.com>
2014-11-09 21:46:24 -0300

    Adicionado função de embaralhar

--- www/js/app.js
+++ www/js/app.js
@@ -31,6 +31,9 @@ angular.module('starter', ['ionic'])
   }
 
   $scope.verificaMovimento = function(direcao, linha, coluna) {
+    if (linha == -1 || coluna == -1) {
+        return false;
+    }
     switch(direcao) {
         case 'left':
             if (coluna - 1 < 0)
@@ -58,6 +61,7 @@ angular.module('starter', ['ionic'])
             return false;
     }
   }
+  
   // efetiva a movimentação, ou seja, muda os determinados valores na matriz
   $scope.efetivaMovimento = function(direcao, linha, coluna) {
      switch(direcao) {
@@ -83,4 +87,91 @@ angular.module('starter', ['ionic'])
              break;
     }
   }
+  
+  // retorna a linha e a coluna onde está o espaço em branco 
+  $scope.encontraEspacoEmbranco = function() {
+	for(i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			if ($scope.matrizJogo[i][j] == "nada") {
+				return [i, j];
+			}
+		}
+	}
+  }
+  
+  // a partir de uma direção retorna a linha e coluna de um quadrado adjacente ao espaço em branco 
+  // capaz de se mover naquela direção ou null caso não seja exista um 
+  $scope.escolheQuadrado = function(direcao) {
+	var posicaoEspacoEmBranco = $scope.encontraEspacoEmbranco();
+	switch (direcao) {
+		case 'left':
+			if (posicaoEspacoEmBranco[1] < 2) {
+				return [posicaoEspacoEmBranco[0], posicaoEspacoEmBranco[1] + 1];
+			}
+			break;
+		case 'right':
+			if (posicaoEspacoEmBranco[1] > 0) {
+				return [posicaoEspacoEmBranco[0], posicaoEspacoEmBranco[1] - 1];
+			}
+			break;
+		case 'up':
+			if (posicaoEspacoEmBranco[0] < 2) {
+				return [posicaoEspacoEmBranco[0] + 1, posicaoEspacoEmBranco[1]];
+			}
+			break;
+		case 'down': 
+			if (posicaoEspacoEmBranco[0] > 0) {
+				return [posicaoEspacoEmBranco[0] - 1, posicaoEspacoEmBranco[1]];
+			}
+			break;
+	}
+	return [-1, -1];
+  }
+  
+  $scope.getDirecaoOposta = function(direcao) {
+	switch (direcao) {
+		case 'left': 
+			return 'right';
+		case 'right':
+			return 'left';
+		case 'up':
+			return 'down';
+		case 'down':  
+			return 'up';
+	}
+  }
+	  
+	
+    $scope.sleep = function(miliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > miliseconds) {
+			break;
+		}
+	}
+    }
+	
+  $scope.embaralhar = function(numeroVezes) {
+	if (cont >= numeroVezes) return;
+	var movimentosPossiveis = ['left', 'right', 'up', 'down'];
+	var direcaoAnterior;
+	var cont = 0;
+	var cont2 = 0;
+	while (cont < numeroVezes && cont2++ < 20) {
+		var rand, direcao;
+		var rand = parseInt(Math.floor(Math.random() * 4)) 
+		var direcao = movimentosPossiveis[rand];
+		var quadradoASeMover = $scope.escolheQuadrado(direcao);
+		//console.log(quadradoASeMover + " ... " + direcao);
+		var movimentoEhValido = direcao != $scope.getDirecaoOposta(direcaoAnterior);
+		var movimentoEhPossivel = $scope.verificaMovimento(direcao, quadradoASeMover[0], quadradoASeMover[1]);
+		if (movimentoEhValido && movimentoEhPossivel) {
+			console.log(direcao);
+			$scope.efetivaMovimento(direcao, quadradoASeMover[0], quadradoASeMover[1]);
+			direcaoAnterior = direcao;
+			cont++;
+		}
+	}
+  }
+  
 });
