3841560c1daf6b289fd5cc4d138e59b9d84914da
Abner M. C. Araujo <abner.math.c@gmail.com>
2014-11-18 16:57:28 -0300

    Adicionado cronometro

---
+++ www/js/cronometro.js
@@ -0,0 +1,16 @@
+function Cronometro(tempoTotal) {
+	this.tempoTotal = tempoTotal;
+	this.tempoAtual = this.tempoTotal;
+};
+
+Cronometro.prototype.diminuiTempo = function() {
+	this.tempoAtual--;
+};
+
+Cronometro.prototype.tempoEsgotado = function() {
+	return this.tempoAtual == 0;
+};
+
+Cronometro.prototype.tempoRestante = function() {
+	return this.tempoAtual / this.tempoTotal;
+};
